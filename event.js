061ca63f1ffb18378143b145fa81efd996f3429d
Emily Chen <emily@truss.works>
2015-09-02 19:03:01 -0700

    Fixed copying error

--- event.js
+++ event.js
@@ -1,36 +1,36 @@
 // Execute the inject.js in a tab and call a method,
 // passing the result to a callback function.
 function injectedMethod (tab, method, callback) {
-  chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
-    chrome.tabs.sendMessage(tab.id, { method: method }, callback);
-  });
+    chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function() {
+        chrome.tabs.sendMessage(tab.id, { method: method }, callback);
+    });
 }
 
 function copyToClipboard( text ){
-                var copyDiv = document.createElement('div');
-                copyDiv.contentEditable = true;
-                document.body.appendChild(copyDiv);
-                copyDiv.innerHTML = text;
-                copyDiv.unselectable = "off";
-                copyDiv.focus();
-                document.execCommand('SelectAll');
-                document.execCommand("Copy", false, null);
-                document.body.removeChild(copyDiv);
-            }
+    var copyDiv = document.createElement('div');
+    copyDiv.contentEditable = true;
+    document.body.appendChild(copyDiv);
+    copyDiv.innerHTML = text;
+    copyDiv.unselectable = "off";
+    copyDiv.focus();
+    document.execCommand('SelectAll');
+    document.execCommand("Copy", false, null);
+    document.body.removeChild(copyDiv);
+}
 
 function getTeam (tab) {
-  // When we get a result back from the getTeam
-  // method, copy the team info to the clipboard
-  injectedMethod(tab, 'getTeam', function (response) {
-    var team = response.data;
-    var mstring = ""
-    for (var i = 0; i < team.length; i++) {
-      mstring += "* " + (team[i].name + " (" + team[i].position + ") vs " + team[i].opponent) + "  \r\n";
-    }
-    copyToClipboard(mstring);
-    alert("Your team has been copied to the clipboard!");
-    return true;
-  });
+    // When we get a result back from the getTeam
+    // method, copy the team info to the clipboard
+    injectedMethod(tab, 'getTeam', function (response) {
+        var team = response.data;
+        var mstring = "";
+        for (var i = 0; i < team.length; i++) {
+            mstring += "* " + (team[i].name + " (" + team[i].position + ") vs " + team[i].opponent) + "  \r\n";
+        }
+        copyToClipboard(mstring);
+        alert("Your team has been copied to the clipboard!");
+        return true;
+    });
 }
 
 // When the browser action is clicked, call the
--- inject.js
+++ inject.js
@@ -1,36 +1,43 @@
-var injected = injected || (function(){
+var injected = injected || (function() {
 
-  var methods = {};
+    var methods = {};
 
-  methods.getTeam = function(){
-    var nodes = document.getElementsByClassName('editable');
-    var players = []
-      // Loop through all the nodes
-  for (var i = 0; i < nodes.length; i++) {
-    var node = nodes[i];
-    // Grabs player info from each node
-    if (node.getElementsByClassName('Nowrap name')[0] != undefined) {
-      var n = node.getElementsByClassName('Nowrap name')[0].textContent;
-      var p = node.getElementsByClassName('Fz-xxs')[0].textContent;
-      if (node.getElementsByClassName('ysf-game-status ')[0].textContent == ' Bye'){
-        var o = 'Bye';
-      } else {
-        var o = node.getElementsByClassName('Inline')[0].textContent;
-      }
-      var player = { name: n, position: p, opponent: o };
-      players.push(player)
-    }
-  }
-    return players;
-  };
+    methods.getTeam = function() {
+        var nodes = document.getElementsByClassName('editable');
+        var players = [];
+        // Loop through all the nodes
+        for (var i = 0; i < nodes.length; i++) {
+            var node = nodes[i];
 
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    var data = {};
-    if (methods.hasOwnProperty(request.method))
-      data = methods[request.method]();
-    sendResponse({ data: data });
+            // Grabs player info from each node
+            if (node.getElementsByClassName('Nowrap name')[0] !== undefined) {
+                var player;
+                var n = node.getElementsByClassName('Nowrap name')[0].textContent;
+                var p = node.getElementsByClassName('Fz-xxs')[0].textContent;
+                var o = '';
+                var matchup = node.getElementsByClassName('F-reset')[0];
+
+                if (matchup && matchup.textContent == ' Bye'){
+                    o = 'Bye';
+                } else {
+                    o = matchup.textContent;
+                }
+
+                player = { name: n, position: p, opponent: o };
+                players.push(player);
+            }
+        }
+        return players;
+    };
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        var data = {};
+        if (methods.hasOwnProperty(request.method)) {
+            data = methods[request.method]();
+        }
+        sendResponse({ data: data });
+        return true;
+    });
+
     return true;
-  });
-
-  return true;
 })();
