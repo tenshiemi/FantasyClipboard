3f1759440674ff455e33a5bcbbe7c210164e70aa
Emily Chen <emily@truss.works>
2016-10-10 17:37:53 -0700

    Fixed bug fetching position

--- inject.js
+++ inject.js
@@ -10,16 +10,18 @@ var injected = injected || (function() {
 
             var player = {};
 
-            var nameNode = node.getElementsByClassName('Nowrap name')[0];
-            var positionNode = node.getElementsByClassName('Fz-xxs')[0];
+            var nameNode = node.getElementsByClassName('ysf-player-name')[0];
+            var name = nameNode.getElementsByClassName('Nowrap name')[0]
+            var positionNode = nameNode.getElementsByClassName('Fz-xxs')[0];
             var gameInfo = node.querySelectorAll('.ysf-game-status > a');
 
             player = {
-                name: nameNode.textContent,
+                name: name.textContent,
                 team: positionNode.textContent.split(' - ')[0],
                 position: positionNode.textContent.split(' - ')[1],
                 opponent: gameInfo.length === 0 ? 'Bye' : gameInfo[1].textContent
             };
+
             players.push(player);
         }
         return players;
